c68464c03b1cf0fad383b045223de40ff8a72c8a
Ayush <ayushmishramay22@gmail.com>
2025-08-25 14:30:16 +0530

    fix cors

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ database.connect();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin:[ 'http://localhost:3000','https://lead-management-woad.vercel.app'],
     credentials: true,
     
  
