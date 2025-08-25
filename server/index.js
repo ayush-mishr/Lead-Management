059e76ec2605ca938dff82f6d725e386463c2c5b
Ayush <ayushmishramay22@gmail.com>
2025-08-25 05:36:47 +0530

    url changed

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ database.connect();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: '*',
+    origin: 'https://lead-management-dumg.onrender.com',
     credentials: true,
     
  
